7450ba647307096142e26c7234e257a82bfb81d4
MochaNeo <yu010213@gmail.com>
2024-01-09 18:54:23 +0900

    todolistの追加

---
+++ src/main/java/com/example/demo/entity/TodoItem.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todoitems")
+@Getter
+@Setter
+public class TodoItem {
+	@Id//プライマリキーとしてidを指定
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//自動的に番号を生成
+    private Long id;//主キー
+	private int category;//仕事か、私生活か、その他か
+    private String title;//タイトル
+    private String description;//詳細
+    private int priority;//優先度
+    private Date createDate;//作成日
+    private Date dueDate;//期日
+    private Boolean done = false;//達成状況
+    
+    
+    //作成日を自動生成する
+    @PrePersist
+    public void prePersist() {
+        this.createDate = new Date(System.currentTimeMillis());
+    }
+}
---
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -0,0 +1,35 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.entity.TodoItem;
+import com.example.demo.service.TodoSearchService;
+
+import jakarta.persistence.EntityManager;
+
+//データベースへアクセスするのクラス
+//JpaRepositoryクラスを継承してentity名と主キーの型を入れる
+public interface TodoRepository extends JpaRepository<TodoItem, Long> {
+	public static final EntityManager entityManager = null;
+	
+	public static final TodoSearchService service = new TodoSearchService();
+	
+	
+	//優先度の降順で完了・未完了のタスクを返す
+    public List<TodoItem> findByDoneOrderByPriorityDesc(boolean done);
+    
+    // JPQLを使用してカスタムのクエリメソッドを定義
+    //WHERE 0=0は真となり全件取得する
+
+    @Query("SELECT b FROM TodoItem b WHERE " +
+    "b.title LIKE %:#{#service.title}% " +
+    "AND (:#{#service.category} = 0 OR b.category = :#{#service.category}) " +
+    "AND (:#{#service.priority} = 0 OR b.priority = :#{#service.priority}) " +
+    "AND b.done = :#{#service.done} " +
+    "ORDER BY b.priority DESC")
+    public List<TodoItem> search(@Param("service") TodoSearchService service);
+}
